54d2e14dbd93b453ecb356fbeba4ec3138490984
unknown <obrienules@gmail.com>
2019-12-17 10:59:59 +0100

    Feat: update application with reselect
    Replace cart selectors with memoized selector

--- src/components/cart.dropdown/cart.dropdown.component.jsx
+++ src/components/cart.dropdown/cart.dropdown.component.jsx
@@ -3,13 +3,14 @@ import "./cart.dropdown.style.scss";
 import CustomButton from "../custom-button/custom.button.component";
 import CartItem from "../cart.item/cart.item.component";
 import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 const CartDropdown = ({ cartItems }) => {
   return (
     <div className="CartDropdown">
       <div className="CartItems">
         {cartItems.map(cartItem => (
-          <CartItem item={cartItem} />
+          <CartItem key={cartItem.id} item={cartItem} />
         ))}
       </div>
       <CustomButton color="dark">CHECKOUT</CustomButton>
@@ -18,6 +19,6 @@ const CartDropdown = ({ cartItems }) => {
 };
 
 const mapStateToProps = state => ({
-  cartItems: state.cart.cartItems
+  cartItems: selectCartItems(state)
 });
 export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart.icon/cart.icon.component.jsx
+++ src/components/cart.icon/cart.icon.component.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./cart.icon.style.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import { connect } from "react-redux";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
-const CartIcon = () => {
+const CartIcon = ({ itemCount }) => {
   return (
     <div className="CartIcon">
       <ShoppingIcon className="ShoppingIcon" />
-      <span className="ItemCount">0</span>
+      <span className="ItemCount">{itemCount}</span>
     </div>
   );
 };
 
-export default CartIcon;
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
+});
+
+export default connect(mapStateToProps, null)(CartIcon);
