f4522fc457997b9b78413b54dcbf82a89ba2b48f
unknown <obrienules@gmail.com>
2019-12-23 21:54:34 +0100

    Add redux-persist to handlel cart items

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -2,9 +2,19 @@ import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
 import headerReducer from "./header/header.reducer";
 import cartReducer from "./cart/cart.reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   header: headerReducer,
   cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./root.reducer";
+import { persistStore } from "redux-persist";
 
 const middleware = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+export const store = createStore(rootReducer, applyMiddleware(...middleware));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
